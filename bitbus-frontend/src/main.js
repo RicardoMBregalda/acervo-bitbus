bf8f642fd5e27376d2621ff9854cb5fdcc6b0aed
RicardoMBregalda <ricardombregalda@gmail.com>
2024-05-16 18:57:13 -0300

    add router v2

--- bitbus-frontend/src/main.js
+++ bitbus-frontend/src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/tailwind.css';
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app');
