5f93ec53afc0f28f941e36aa1d45417fc0c45a3c
RicardoMBregalda <ricardombregalda@gmail.com>
2024-05-25 00:53:07 -0300

    visitante form com cpf/cnpj dinamicos

--- bitbus-frontend/src/views/visitante/VisitanteForm.vue
+++ bitbus-frontend/src/views/visitante/VisitanteForm.vue
@@ -1,22 +1,36 @@
 <script setup>
+import { ref, watch } from 'vue';
 import Sidebar from '../../components/Sidebar.vue';
 
+const tipo = ref('PF');
+const identificacaoLabel = ref('CPF');
+const identificacaoPlaceholder = ref('000.000.000-00');
+
+watch(tipo, (newValue) => {
+    if (newValue === 'PJ') {
+        identificacaoLabel.value = 'CNPJ';
+        identificacaoPlaceholder.value = '00.000.000/0000-00';
+    } else {
+        identificacaoLabel.value = 'CPF';
+        identificacaoPlaceholder.value = '000.000.000-00';
+    }
+});
 </script>
 
 <template>
-
     <Sidebar />
 
-    <div class=" p-10 sm:ml-64 relative overflow-x-auto">
-
+    <div class="p-10 sm:ml-64 relative overflow-x-auto">
         <div class="max-w-lg grid mx-auto grid-cols-12">
-            <h1 class=" col-span-12 text-3xl text-teal-900 dark:text-white mb-5"> Cadastrar Visitante</h1>
+            <h1 class="col-span-12 text-3xl text-teal-900 dark:text-white mb-5">Cadastrar Visitante</h1>
+
             <div class="mb-5 col-span-12">
                 <label for="name" class="block mb-2 text-sm font-medium text-teal-900 dark:text-white">Nome</label>
                 <input type="text" id="name"
                     class="shadow-sm bg-teal-50 border border-teal-300 text-teal-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-teal-700 dark:border-teal-600 dark:placeholder-teal-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500 dark:shadow-sm-light"
                     placeholder="Fulano de Tal" required />
             </div>
+
             <div class="mb-5 col-span-12">
                 <label for="email" class="block mb-2 text-sm font-medium text-teal-900 dark:text-white">Email</label>
                 <input type="email" id="email"
@@ -25,29 +39,27 @@ import Sidebar from '../../components/Sidebar.vue';
             </div>
 
             <div class="mb-5 col-span-2">
-                <label for="repeat-password"
-                    class="block mb-2 text-sm font-medium text-teal-900 dark:text-white">tipo</label>
-                <select id="countries"
+                <label for="tipo" class="block mb-2 text-sm font-medium text-teal-900 dark:text-white">Tipo</label>
+                <select id="tipo" v-model="tipo"
                     class="bg-teal-50 border border-teal-300 text-teal-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-teal-700 dark:border-teal-600 dark:placeholder-teal-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500">
-                    >
                     <option value="PF">PF</option>
                     <option value="PJ">PJ</option>
                 </select>
             </div>
+
             <div class="mb-5 col-start-4 col-span-9">
-                <label for="repeat-password"
-                    class="block mb-2 text-sm font-medium text-teal-900 dark:text-white">CPF</label>
-                <input type="password" id="repeat-password"
+                <label :for="identificacaoLabel.toLowerCase()"
+                    class="block mb-2 text-sm font-medium text-teal-900 dark:text-white">{{ identificacaoLabel
+                    }}</label>
+                <input type="text" :id="identificacaoLabel.toLowerCase()" :placeholder="identificacaoPlaceholder"
                     class="shadow-sm bg-teal-50 border border-teal-300 text-teal-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-teal-700 dark:border-teal-600 dark:placeholder-teal-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500 dark:shadow-sm-light"
                     required />
             </div>
 
-
             <button type="submit"
-                class="mt-6 col-span-12 text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">Salvar</button>
-
+                class="mt-6 col-span-12 text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">
+                Salvar
+            </button>
         </div>
-
     </div>
-
 </template>
