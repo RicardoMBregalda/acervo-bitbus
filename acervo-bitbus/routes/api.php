f5d023210a537efa027b22f32cf3d0cfb240b180
lorenzomene <lorenzospmene@gmail.com>
2024-06-05 20:37:57 -0300

    feat: add new routes

--- acervo-bitbus/routes/api.php
+++ acervo-bitbus/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/visita', 'VisitaController@store');
+Route::get('/visita/{id}', 'VisitaController@show');
+Route::put('/visita/{id}', 'VisitaController@update');
+Route::delete('/visita/{id}', 'VisitaController@delete');
+// Route::get('/visita', 'VisitaController@index');
