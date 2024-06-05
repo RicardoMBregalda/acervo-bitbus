662cd79a2befed99d3218977a0daee224a239ea2
lorenzomene <lorenzospmene@gmail.com>
2024-06-05 16:10:24 -0300

    feat: doacoes table

---
+++ acervo-bitbus/database/migrations/2024_06_05_142809_create_doacoes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doacoes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('tipo_doacao');
+            $table->decimal('valor', 11, 2);
+            $table->foreignId('participante_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doacoes');
+    }
+};
