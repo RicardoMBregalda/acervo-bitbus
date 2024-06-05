cc931778143572403446eade451da42f12f98588
lorenzomene <lorenzospmene@gmail.com>
2024-06-05 16:09:33 -0300

    feat: visitas table

---
+++ acervo-bitbus/database/migrations/2024_06_05_112503_create_visitas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visitas', function (Blueprint $table) {
+            $table->id();
+            $table->string('organizador');
+            $table->text('endereco');
+            $table->string('complemento')->nullable();
+            $table->text('descricao');
+            $table->date('data_inicio')->index();
+            $table->date('data_fim')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visitas');
+    }
+};
