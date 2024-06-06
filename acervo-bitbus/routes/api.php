f4a7cf503e2edd9d0fb71c4bbf2bc1ca5ce4bd4f
lorenzomene <lorenzospmene@gmail.com>
2024-06-05 21:47:57 -0300

    fix: routes fix

--- acervo-bitbus/routes/api.php
+++ acervo-bitbus/routes/api.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VisitaController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::post('/visita', 'VisitaController@store');
-Route::get('/visita/{id}', 'VisitaController@show');
-Route::put('/visita/{id}', 'VisitaController@update');
-Route::delete('/visita/{id}', 'VisitaController@delete');
-// Route::get('/visita', 'VisitaController@index');
+Route::post('visita', [VisitaController::class, 'store']);
+Route::get('visita', [VisitaController::class, 'index']);
+Route::get('visita/{id}', [VisitaController::class, 'show']);
+Route::put('visita/{id}', [VisitaController::class, 'update']);
+Route::delete('visita/{id}', [VisitaController::class, 'delete']);
