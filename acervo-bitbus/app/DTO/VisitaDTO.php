aef4960138ad94d0969ef9544e93dad72712ae01
lorenzomene <lorenzospmene@gmail.com>
2024-06-05 21:49:54 -0300

    fix: remove readonly properties

--- acervo-bitbus/app/DTO/VisitaDTO.php
+++ acervo-bitbus/app/DTO/VisitaDTO.php
@@ -4,21 +4,23 @@ namespace App\DTO;
 
 use App\Core\DTO\BaseDTO;
 
-class VisitaDTO implements BaseDTO
+class VisitaDTO extends BaseDTO
 {
-    public readonly string $organizador;
-    public readonly string $endereco;
-    public readonly string $complemento;
-    public readonly string $descricao;
-    public readonly string $data_inicio;
-    public readonly string $data_fim;
+    public string $organizador;
+    public string $endereco;
+    public string $cidade;
+    public string $estado;
+    public string $descricao;
+    public string $data_inicio;
+    public string $data_fim;
 
     public static function fromValidatedData(array $validatedData): self
     {
         return new self([
             'organizador' => $validatedData['organizador'],
             'endereco' => $validatedData['endereco'],
-            'complemento' => $validatedData['complemento'],
+            'cidade' => $validatedData['cidade'],
+            'estado' => $validatedData['estado'],
             'descricao' => $validatedData['descricao'],
             'data_inicio' => $validatedData['data_inicio'],
             'data_fim' => $validatedData['data_fim'],
