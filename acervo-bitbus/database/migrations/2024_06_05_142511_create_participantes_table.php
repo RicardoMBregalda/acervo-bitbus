f55f17765951628598339fe627f14bdcdce947af
RicardoMBregalda <ricardombregalda@gmail.com>
2024-06-05 22:36:44 -0300

    fix: table participantes

--- acervo-bitbus/database/migrations/2024_06_05_142511_create_participantes_table.php
+++ acervo-bitbus/database/migrations/2024_06_05_142511_create_participantes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('participantes', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
+            $table->string('tipo');
             $table->string('documento');
             $table->string('email');
             $table->timestamps();
