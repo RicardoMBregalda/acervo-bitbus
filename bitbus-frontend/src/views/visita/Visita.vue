194b8fab56724aceeeb6614e7bd5e67c694d84f3
RicardoMBregalda <ricardombregalda@gmail.com>
2024-06-05 23:29:38 -0300

    fix: remove pagination

--- bitbus-frontend/src/views/visita/Visita.vue
+++ bitbus-frontend/src/views/visita/Visita.vue
@@ -103,43 +103,6 @@ onMounted(async () => {
       </table>
     </div>
 
-    <nav class="mx-4 flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
-      aria-label="Table navigation">
-      <span
-        class=" text-sm font-normal text-teal-500 dark:text-teal-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing
-        <span class="font-semibold text-teal-900 dark:text-white">1-10</span> of <span
-          class="font-semibold text-teal-900 dark:text-white">1000</span></span>
-      <ul class=" inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-        <li>
-          <a href="#"
-            class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-teal-500 bg-white border border-teal-300 rounded-s-lg hover:bg-teal-100 hover:text-teal-700 dark:bg-teal-800 dark:border-teal-700 dark:text-teal-400 dark:hover:bg-teal-700 dark:hover:text-white">Previous</a>
-        </li>
-        <li>
-          <a href="#"
-            class="flex items-center justify-center px-3 h-8 leading-tight text-teal-500 bg-white border border-teal-300 hover:bg-teal-100 hover:text-teal-700 dark:bg-teal-800 dark:border-teal-700 dark:text-teal-400 dark:hover:bg-teal-700 dark:hover:text-white">1</a>
-        </li>
-        <li>
-          <a href="#"
-            class="flex items-center justify-center px-3 h-8 leading-tight text-teal-500 bg-white border border-teal-300 hover:bg-teal-100 hover:text-teal-700 dark:bg-teal-800 dark:border-teal-700 dark:text-teal-400 dark:hover:bg-teal-700 dark:hover:text-white">2</a>
-        </li>
-        <li>
-          <a href="#" aria-current="page"
-            class="flex items-center justify-center px-3 h-8 text-teal-600 border border-teal-300 bg-teal-50 hover:bg-teal-100 hover:text-teal-700 dark:border-teal-700 dark:bg-teal-700 dark:text-white">3</a>
-        </li>
-        <li>
-          <a href="#"
-            class="flex items-center justify-center px-3 h-8 leading-tight text-teal-500 bg-white border border-teal-300 hover:bg-teal-100 hover:text-teal-700 dark:bg-teal-800 dark:border-teal-700 dark:text-teal-400 dark:hover:bg-teal-700 dark:hover:text-white">4</a>
-        </li>
-        <li>
-          <a href="#"
-            class="flex items-center justify-center px-3 h-8 leading-tight text-teal-500 bg-white border border-teal-300 hover:bg-teal-100 hover:text-teal-700 dark:bg-teal-800 dark:border-teal-700 dark:text-teal-400 dark:hover:bg-teal-700 dark:hover:text-white">5</a>
-        </li>
-        <li>
-          <a href="#"
-            class="flex items-center justify-center px-3 h-8 leading-tight text-teal-500 bg-white border border-teal-300 rounded-e-lg hover:bg-teal-100 hover:text-teal-700 dark:bg-teal-800 dark:border-teal-700 dark:text-teal-400 dark:hover:bg-teal-700 dark:hover:text-white">Next</a>
-        </li>
-      </ul>
-    </nav>
 
   </div>
 
