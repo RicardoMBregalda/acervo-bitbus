6aec21c4d407a33c3ff108074de48fe0755bbbb9
RicardoMBregalda <ricardombregalda@gmail.com>
2024-07-02 22:24:27 -0300

    fix tela de listagem

--- bitbus-frontend/src/views/feedback/Feedback.vue
+++ bitbus-frontend/src/views/feedback/Feedback.vue
@@ -35,40 +35,43 @@ const buscarFeedback = () => {
     <Sidebar />
 
     <div class="flex flex-col top-3.5 sm:ml-64 relative overflow-x-auto pr-10">
-
-
+        <h1
+            class="mx-4 col-span-12 text-3xl text-green-900 dark:text-white pb-1 mb-4 space-y-2 font-medium border-b border-green-300 dark:border-green-700">
+            Feedback</h1>
         <!-- Deixei para testes, versão final vamos tirar esse campo  -->
-        <div class="flex justify-end mx-4">
-            <router-link to="/feedback/form">
-                <button type="button"
-                    class="px-6 py-3.5 text-base font-medium text-white inline-flex items-center bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-lg text-center dark:bg-green-500 dark:hover:bg-green-800 dark:focus:ring-green-800">
-                    Cadastrar
-                </button>
-            </router-link>
-        </div>
+        <div class="flex justify-between">
 
-        <div class="mx-4 flex space-y-4 sm:space-y-0 justify-start">
-            <label for="table-search"
-                class="mb-2 text-sm font-medium text-green-900 sr-only dark:text-white">Search</label>
-            <div class="relative">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg class="w-5 h-5 text-green-500 dark:text-green-400" aria-hidden="true" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                            clip-rule="evenodd"></path>
-                    </svg>
+
+            <div class="mx-4 flex space-y-4 sm:space-y-0 ">
+                <label for="table-search"
+                    class="mb-2 text-sm font-medium text-green-900 sr-only dark:text-white">Search</label>
+                <div class="relative">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-5 h-5 text-green-500 dark:text-green-400" aria-hidden="true" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <input type="text" id="table-search" v-model="buscar" @keyup.enter="buscarFeedback"
+                        class="mr-20 block w-full p-4 ps-10 text-sm text-green-900 border border-green-300 rounded-lg bg-green-50 focus:ring-green-500 focus:border-green-500 dark:bg-green-700 dark:border-green-500 dark:placeholder-green-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                        placeholder="Busque pela avaliação">
+                    <button
+                        class="text-white absolute end-2.5 bottom-2.5 bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-500 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                        @click="buscarFeedback">Buscar</button>
                 </div>
-                <input type="text" id="table-search" v-model="buscar" @keyup.enter="buscarFeedback"
-                    class="mr-20 block w-full p-4 ps-10 text-sm text-green-900 border border-green-300 rounded-lg bg-green-50 focus:ring-green-500 focus:border-green-500 dark:bg-green-700 dark:border-green-500 dark:placeholder-green-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
-                    placeholder="Busque pela avaliação">
-                <button
-                    class="text-white absolute end-2.5 bottom-2.5 bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-500 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                    @click="buscarFeedback">Buscar</button>
+            </div>
+            <div class="flex  mx-4">
+                <router-link to="/feedback/form">
+                    <button type="button"
+                        class="px-6 py-3.5 text-base font-medium text-white inline-flex items-center bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-lg text-center dark:bg-green-500 dark:hover:bg-green-800 dark:focus:ring-green-800">
+                        Cadastrar
+                    </button>
+                </router-link>
             </div>
         </div>
-
-        <div class="m-4 rounded-lg overflow-hidden border border-green-500">
+        <div class="m-4 rounded-lg overflow-hidden border border-green-300">
             <table class="w-full text-sm text-left rtl:text-right text-green-500 dark:text-green-400">
                 <thead class="text-xs text-green-700 uppercase bg-green-50 dark:bg-green-700 dark:text-green-50">
 
