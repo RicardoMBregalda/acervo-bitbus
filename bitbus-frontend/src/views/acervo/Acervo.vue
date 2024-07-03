f67a27e0c2ee3187261e36fa804b8cce6b193a35
RicardoMBregalda <ricardombregalda@gmail.com>
2024-07-02 23:39:22 -0300

    ajuste

--- bitbus-frontend/src/views/acervo/Acervo.vue
+++ bitbus-frontend/src/views/acervo/Acervo.vue
@@ -71,7 +71,7 @@ const buscarProduto = () => {
           </div>
           <input type="text" id="table-search" v-model="buscar" @keyup.enter="buscarProduto"
             class="mr-20 block w-full p-4 ps-10 text-sm text-green-900 border border-green-300 rounded-lg bg-green-50 focus:ring-green-500 focus:border-green-500 dark:bg-green-500 dark:border-green-600 dark:placeholder-green-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
-            placeholder="Busque pelo código">
+            placeholder="Busque pelo nome">
           <button
             class="text-white absolute end-2.5 bottom-2.5 bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-500 dark:focus:ring-green-800"
             @click="buscarProduto">Buscar</button>
