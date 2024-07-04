bcbf7e39c48956e0a1be3233cbcd205a10440df0
Ricardo Bregalda <ricardombregalda@gmail.com>
2024-07-04 20:06:01 -0300

    fix: alteracao do icone de sair

--- bitbus-frontend/src/components/Sidebar.vue
+++ bitbus-frontend/src/components/Sidebar.vue
@@ -139,17 +139,16 @@
             to=""
             class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-green-600 dark:hover:bg-green-800 group">
             <svg
-              class="flex-shrink-0 w-5 h-5 text-white transition duration-75 dark:text-green-400 group-hover:text-white dark:group-hover:text-green-200"
-              aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              viewBox="0 0 24 24">
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="flex-shrink-0 w-5 h-5 text-white transition duration-75 dark:text-green-400 group-hover:text-white dark:group-hover:text-green-200">
               <path
-                fill-rule="evenodd"
-                d="M9.484 6.743c.41-.368.443-1 .077-1.41a.99.99 0 0 0-1.405-.078L2.67 10.203l-.007.006A2.048 2.048 0 0 0 2 11.721a2.058 2.058 0 0 0 .662 1.51l5.584 5.09a.99.99 0 0 0 1.405-.07 1.003 1.003 0 0 0-.07-1.412l-5.577-5.082a.05.05 0 0 1 0-.072l5.48-4.942Zm6.543 9.199v-.42a4.168 4.168 0 0 1 2.715 2.415c.154.382.44.695.806.88a1.683 1.683 0 0 0 2.167-.571c.214-.322.312-.707.279-1.092V15.88c0-3.77-2.526-7.039-5.966-7.573V7.57a1.957 1.957 0 0 0-.994-1.838 1.931 1.931 0 0 0-2.153.184L7.8 10.164a.753.753 0 0 0-.012.011l-.011.01a2.098 2.098 0 0 0-.703 1.57 2.108 2.108 0 0 0 .726 1.59l5.08 4.25a1.933 1.933 0 0 0 2.929-.614c.167-.32.242-.68.218-1.04Z"
-                clip-rule="evenodd" />
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
             </svg>
 
             <span class="flex-1 ms-3 whitespace-nowrap">Sair</span>
